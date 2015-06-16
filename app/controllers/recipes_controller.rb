634b591acc91d75fc396296616c06c9d594ffd41
Guilherme Figueiroa <guilherme.mfigueiroa@gmail.com>
2015-06-16 00:03:04 -0300

    Add models

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,34 @@
 class RecipesController < ApplicationController
   def new
+  	@recipe = Recipe.new
   end
+
+  def index
+  	@recipes = Recipe.all
+  end
+
+  def show
+  	@recipe = Recipe.find(params[:id])
+  end
+
+  def create
+  	@recipe = Recipe.new(recipe_params)
+  	if @recipe.save
+  		flash[:success] = "Receita salva com sucesso"
+  		redirect_to recipes_path
+  	else
+  		flash[:error] = "Erro ao salvar receita"
+  		render 'new'
+  	end
+  end
+
+  def destroy
+  	@recipe = Recipe.find(params[:id])
+  	@recipe.destroy
+  end
+
+  private
+  	def recipe_params
+  		params.require(:recipe).permit(:name, :description, :cooking_time)
+  	end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,17 @@
 class Recipe < ActiveRecord::Base
-	has_many :ingredients
-	has_many :prep_steps
+	has_many :ingredients, :dependent => :destroy
+	has_many :prep_steps, :dependent => :destroy
+	accepts_nested_attributes_for :ingredients
+	accepts_nested_attributes_for :prep_steps
+	validates :name, presence: true
+
+	def cooking_time_formatted
+		if self.cooking_time.present?
+			if self.cooking_time.strftime("%H").to_f > 0 
+				self.cooking_time.strftime "%H horas e %M minutos"
+			else
+				self.cooking_time.strftime "%M minutos"
+			end
+		end
+	end
 end
