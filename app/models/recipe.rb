a5ee281804aeecf6ea015d78ace473d44654a5d0
guifigueiroa <guilhermefigueiroa@MacBook-Air-de-Guilherme.local>
2015-07-07 22:53:10 -0300

    Fix has_many through for recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients, dependent: :destroy
   has_many :prep_steps, dependent: :destroy
   has_many :hmms
+  has_many :users, through: :hmms
   validates :name, presence: true, length: { maximum: 50 }
   validates :cooking_time, presence: :true
   has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "400x400" }
