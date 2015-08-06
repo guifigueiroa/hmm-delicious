3794a57be512ba80c341584fccb1fe8a82e84ba7
Guilherme Figueiroa <guilherme.mfigueiroa@gmail.com>
2015-08-05 23:07:22 -0300

    Fix for broken new recipe page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -18,6 +18,10 @@ class RecipesController < ApplicationController
     recipe
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user = current_user
